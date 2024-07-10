01758368be09d644166a06019ba9bf6f9f53f5d3
Fankhauserli <linus.fankhauser@proton.me>
2024-07-10 15:20:49 +0000

    New Version: 3.2.12

--- src/tools/chocolateyinstall.ps1
+++ src/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 
-$URL='https://github.com/ProtonVPN/win-app/releases/download/3.2.11/ProtonVPN_v3.2.11.exe'
-$checksum='eb37bf757a2613e6b5c0d328e9b27c64902249c510d8883593e83ff289794aa0'
+$URL='https://github.com/ProtonVPN/win-app/releases/download/3.2.12/ProtonVPN_v3.2.12.exe'
+$checksum='a6b9e7faa5c41c04f71634570959415ca36c188fc761287c69f67757d90ef277'
 
 $packageArgs = @{
   packageName    = 'protonvpn-fankhauserli'
